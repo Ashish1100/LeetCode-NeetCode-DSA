f9837a1d2a8cecb41fdf704f6827985453e65d9e
Ashish Saha <97810143+Ashish1100@users.noreply.github.com>
2024-04-25 17:54:15 +0530

    Update containsDuplicate.cpp

--- DSA Problems/Day-1/Solution/containsDuplicate.cpp
+++ DSA Problems/Day-1/Solution/containsDuplicate.cpp
@@ -1,1 +1,23 @@
+// function: Naive Approach
 
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) 
+    {
+        
+     sort(nums.begin(), nums.end()); 
+     int n = nums.size();
+
+    for (int i =0; i < n-1; i++)
+     {
+        if(nums[i] == nums[i+1])
+        {
+          return true;
+        }
+     }
+  return false;
+    }
+};
+
+// TC: O(nlogn)
+// SC: O(1)
